95b99588745e79e44e898cd43eac792a548f4938
Luis Barata da Fonseca Magalhaes/IT/CSUK <Luis.Magalhaes@creditsafe.com>
2020-08-15 20:35:28 +0100

    Lambda api entry point creation

---
+++ api.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Api = require("claudia-api-builder");
+const api = new Api();
+
+api.get("/pizzas", () => {
+  return ["Capricciosa", "Quattro Formagi", "Napoletana", "Margherita"];
+});
+
+module.exports = api;
+
+//"https://etyz7t1l05.execute-api.us-east-2.amazonaws.com/latest"
