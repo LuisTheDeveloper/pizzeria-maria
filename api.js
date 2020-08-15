2f7fdd3ef11e6f993a3f07e31d6a7efe11118de0
Luis Barata da Fonseca Magalhaes/IT/CSUK <Luis.Magalhaes@creditsafe.com>
2020-08-15 21:22:12 +0100

    Handler and data creation. List of pizzas and list by id handler

--- api.js
+++ api.js
@@ -3,11 +3,22 @@
 const Api = require("claudia-api-builder");
 const api = new Api();
 
+const getPizzas = require("./handlers/getPizzas");
+
+api.get("/", () => "Welcome to the API Pizza!");
+
 api.get("/pizzas", () => {
-  return ["Capricciosa", "Quattro Formagi", "Napoletana", "Margherita"];
+  return getPizzas();
 });
+
+api.get(
+  "/pizzas/{id}",
+  (request) => {
+    return getPizzas(request.pathParams.id);
+  },
+  { error: 404 }
+);
 
 module.exports = api;
 
 //"https://etyz7t1l05.execute-api.us-east-2.amazonaws.com/latest"
-//by
